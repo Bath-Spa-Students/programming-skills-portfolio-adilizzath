255354816635d622405401829d4b5df183850d92
adilizzath <145958462+adilizzath@users.noreply.github.com>
2023-10-12 13:27:40 +0400

    Create practice3.py

---
+++ Chapter 1- Getting Started with Python/Exercises/practice3.py
@@ -0,0 +1,5 @@
+#you need to makea program for a leader board.the program needs to output the numbers 1 to 9,each on saperate line followed by a dot
+
+print("welcome to the leader board")
+for i in range(1,10):
+    print(i,".")
