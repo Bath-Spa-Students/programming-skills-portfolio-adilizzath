a4712cb834b3a161c334dda6caf38c26322d2ca2
adilizzath <145958462+adilizzath@users.noreply.github.com>
2023-10-12 13:27:48 +0400

    Create practice5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/practice5.py
@@ -0,0 +1,8 @@
+#write four strings in different variables and print the output as one string(string concatenation)
+
+a= "mohamed"
+b= "adil"
+c= "creative computing"
+d= "bathspa"
+
+print(a,b,c,d)
