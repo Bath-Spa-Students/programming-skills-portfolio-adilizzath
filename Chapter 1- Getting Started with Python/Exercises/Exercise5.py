e3f778c3a7e8dd3a077ca91d18546e9db4061933
adilizzath <145958462+adilizzath@users.noreply.github.com>
2023-11-10 20:17:00 +0400

    Update Exercise5.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise5.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise5.py
@@ -1,3 +1,8 @@
-r=int(input("enter the circle radius"))
-area=22/7*r*r
-print("the area of the given circle radius is",area)
+# Taking user input for the circle radius
+r = int(input("Enter the circle radius: "))
+
+# Calculating the area of the circle using the formula: area = π * r^2
+area = 22/7 * r * r
+
+# Printing the calculated area
+print("The area of the given circle with radius", r, "is", area)
