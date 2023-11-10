b6ee50d6f90ad942ef167b81242f57a242366d6e
adilizzath <145958462+adilizzath@users.noreply.github.com>
2023-11-10 17:06:10 +0400

    Update Exercise5.py

--- Chapter 4- Control Flow/Exercises/Exercise5.py
+++ Chapter 4- Control Flow/Exercises/Exercise5.py
@@ -1,16 +1,21 @@
 favorite_fruits = ["watermelon", "mango", "strawberry"]
 
+# Check if "mango" is in the favorite_fruits list.
 if "mango" in favorite_fruits:
     print("You really like mango!")
 
+# Check if "watermelon" is in the favorite_fruits list.
 if "watermelon" in favorite_fruits:
     print("You really like watermelon!")
 
+# Check if "banana" is in the favorite_fruits list.
 if "banana" in favorite_fruits:
     print("You really like banana!")
 
+# Check if "strawberry" is in the favorite_fruits list.
 if "strawberry" in favorite_fruits:
-    print("You really like strawberries!")
+    print("You really like strawberry!")
 
+# Check if "pineapples" is in the favorite_fruits list.
 if "pineapples" in favorite_fruits:
-    print("You really like pineapples!")
+    print("You really like pineapple!")
