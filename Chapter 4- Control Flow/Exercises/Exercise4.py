c9df03ae60a3bc6a02042405895acd704b36b669
adilizzath <145958462+adilizzath@users.noreply.github.com>
2023-11-10 17:05:36 +0400

    Update Exercise4.py

--- Chapter 4- Control Flow/Exercises/Exercise4.py
+++ Chapter 4- Control Flow/Exercises/Exercise4.py
@@ -1,14 +1,14 @@
-age = 30  # You can change this value to test different age 
+age = 30  # You can change this value to test different ages.
 
 if age < 2:
-    print("The person is a baby.")
+    print("The person is a baby.")  # If age is less than 2, the person is considered a baby.
 elif age < 4:
-    print("The person is a toddler.")
+    print("The person is a toddler.")  # If age is between 2 (inclusive) and 4 (exclusive), the person is a toddler.
 elif age < 13:
-    print("The person is a kid.")
+    print("The person is a kid.")  # If age is between 4 (inclusive) and 13 (exclusive), the person is a kid.
 elif age < 20:
-    print("The person is a teenager.")
+    print("The person is a teenager.")  # If age is between 13 (inclusive) and 20 (exclusive), the person is a teenager.
 elif age < 65:
-    print("The person is an adult.")
+    print("The person is an adult.")  # If age is between 20 (inclusive) and 65 (exclusive), the person is an adult.
 else:
-    print("The person is an elder.")
+    print("The person is an elder.")  # If age is 65 or older, the person is considered an elder.
