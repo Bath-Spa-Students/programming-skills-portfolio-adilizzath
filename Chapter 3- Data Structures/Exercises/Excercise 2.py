6fc86140452bce295cd84aeb8ec08f3482c9c9bf
adilizzath <145958462+adilizzath@users.noreply.github.com>
2023-11-10 19:44:37 +0400

    Update Exercise 2.py

--- Chapter 3- Data Structures/Exercises/Excercise 2.py
+++ Chapter 3- Data Structures/Exercises/Excercise 2.py
@@ -1,9 +1,15 @@
-names = ["mohamed", "adhil", "issathulla","ahmed"]
-msg = "Hello, " + names[0].title() + "!"
+# List of names
+names = ["mohamed", "adhil", "issathulla", "ahmed"]
+
+# Greeting each person in the list
+msg = "Hello, " + names[0].title() + "!"  # Greeting the first person in the list
 print(msg)
-msg = "Hello, " + names[1].title() + "!"
+
+msg = "Hello, " + names[1].title() + "!"  # Greeting the second person in the list
 print(msg)
-msg = "Hello, " + names[2].title() + "!"
+
+msg = "Hello, " + names[2].title() + "!"  # Greeting the third person in the list
 print(msg)
-msg = "Hello, " + names[3].title() + "!"
+
+msg = "Hello, " + names[3].title() + "!"  # Greeting the fourth person in the list
 print(msg)
