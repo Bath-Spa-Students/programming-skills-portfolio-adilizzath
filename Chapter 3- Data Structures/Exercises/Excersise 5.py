102ad0c1521e6248bb3f62426aaaeddb429264fd
adilizzath <145958462+adilizzath@users.noreply.github.com>
2023-11-10 19:57:10 +0400

    Update Excersise 5.py

--- Chapter 3- Data Structures/Exercises/Excersise 5.py
+++ Chapter 3- Data Structures/Exercises/Excersise 5.py
@@ -1,24 +1,26 @@
-names=["adhil","Alen","Karthikeyan","Shaan"]
+# First section: Inviting guests using a loop
+names = ["adhil", "Alen", "Karthikeyan", "Shaan"]
 for i in names:
-    print(i,",We are galdly inviting you to join us on a dinner party")
+    print(i + ", we are gladly inviting you to join us at a dinner party.")
 
-#or
+# Second section: Inviting guests individually with messages
+Guests = ["adhil", "shad", "rubi"]
+msg1 = "Hello " + Guests[0] + ", you are invited to the dinner."
+msg2 = "Hello " + Guests[1] + ", you are invited to the dinner."
+msg3 = "Hello " + Guests[2] + ", you are invited to the dinner."
 
-Guests=["adhil","shad","rubi"]
-msg1=("hello "+ Guests[0]+",you are invited to the dinner")
-msg2=("hello "+ Guests[1]+",you are invited to the dinner")
-msg3=("hello "+ Guests[2]+",you are invited to the dinner")
-
+# Printing individual invitations
 print(msg1)
 print(msg2)
 print(msg3)
 
-print("\nsadly "+Guests[2]+",cannot come to the party")
-Guests[2]="rubi"
+# Modifying the guest list and updating an invitation message
+print("\nSadly, " + Guests[2] + " cannot come to the party.")
+Guests[2] = "rubi"  # Correcting the name
 
-msg1=("\nhello "+Guests[0]+",you are invited to the dinner")
-msg2=("\nhello "+Guests[1]+",you are invited to the dinner")
+msg1 = "\nHello " + Guests[0] + ", you are invited to the dinner."
+msg2 = "\nHello " + Guests[1] + ", you are invited to the dinner."
 
+# Printing updated invitations
 print(msg1)
 print(msg2)
-
