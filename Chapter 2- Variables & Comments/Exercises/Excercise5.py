6d3b104b50dfc95fa91c7e42ab0da39eceec39d0
adilizzath <145958462+adilizzath@users.noreply.github.com>
2023-11-10 20:01:42 +0400

    Update Excercise5.py

--- Chapter 2- Variables & Comments/Exercises/Excercise5.py
+++ Chapter 2- Variables & Comments/Exercises/Excercise5.py
@@ -1,14 +1,15 @@
-#A girl heads to a computer shop to buy some USB sticks. She loves USB sticks and wants as many as she can get for £50. They are £6 each.
+# Price of a USB stick in pounds
+usb_price = 6
 
-#Write a programme that calculates how many USB sticks she can buy and how many pounds she will have left.
+# Amount in pounds the person has
+pound = 50
 
-#You will to use the arithmetic operators to complete this exercise.
+# Calculating how many USB sticks can be bought
+shecanget = pound // usb_price
 
-usb_price=6
-pound=50
+# Calculating the amount of change the person will receive
+p_change = pound - shecanget * usb_price
 
-shecanget=pound//usb_price
-p_change=pound-shecanget
-
-print("sticks she can buy :",shecanget)
-print("The amount she will get back:",p_change)
+# Printing the results
+print("Sticks she can buy:", shecanget)
+print("The amount she will get back:", p_change)
