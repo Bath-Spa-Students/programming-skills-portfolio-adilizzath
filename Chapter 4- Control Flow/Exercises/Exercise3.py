9e5ae1afc88c738a1d4966a0b758b81efe8ca72a
adilizzath <145958462+adilizzath@users.noreply.github.com>
2023-11-10 17:04:54 +0400

    Update Exercise3.py

--- Chapter 4- Control Flow/Exercises/Exercise3.py
+++ Chapter 4- Control Flow/Exercises/Exercise3.py
@@ -1,33 +1,34 @@
 #VERSION 1
-alien_color = "green"
 
-if alien_color == "green":
-    print("The player has earned 5 points.")
-elif alien_color == "yellow":
-    print("The player has earned 10 points.")
-else:
-    print("The player has earned 15 points.")
+alien_color = "green"  # Set the variable 'alien_color' to "green".
+
+if alien_color == "green":  # Check if 'alien_color' is "green".
+    print("The player has earned 5 points.")  # If 'alien_color' is "green," print this message.
+elif alien_color == "yellow":  # If 'alien_color' is not "green," check if it's "yellow."
+    print("The player has earned 10 points.")  # If 'alien_color' is "yellow," print this message.
+else:  # If 'alien_color' is neither "green" nor "yellow," execute this block.
+    print("The player has earned 15 points.")  # Print this message.
 
 #VERSION 2
 
-alien_color = "yellow"
+alien_color = "yellow"  # Set the variable 'alien_color' to "yellow".
 
-if alien_color == "green":
-    print("The player has earned 5 points.")
-elif alien_color == "yellow":
-    print("The player has earned 10 points.")
-else:
-    print("The player has earned 15 points.")
+if alien_color == "green":  # Check if 'alien_color' is "green".
+    print("The player has earned 5 points.")  # If 'alien_color' is "green," print this message.
+elif alien_color == "yellow":  # If 'alien_color' is not "green," check if it's "yellow."
+    print("The player has earned 10 points.")  # If 'alien_color' is "yellow," print this message.
+else:  # If 'alien_color' is neither "green" nor "yellow," execute this block.
+    print("The player has earned 15 points.")  # Print this message.
 
 #VERSION 3
 
-alien_color = "red"
 
+alien_color = "red" #Assigning the alien's color to the variable 'alien_color'
+
+# Checking the value of 'alien_color' and printing points accordingly
 if alien_color == "green":
     print("The player earned 5 points.")
 elif alien_color == "yellow":
     print("The player earned 10 points.")
-else:
+else: # If 'alien_color' is neither green nor yellow, the player earns 15 points
     print("The player earned 15 points.")
-
-
